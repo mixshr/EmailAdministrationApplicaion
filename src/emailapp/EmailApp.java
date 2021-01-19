a9f56061b965912c5f0cc497cbb061e2c009350b
mixshr <mixail.sharenko@gmail.com>
2021-01-19 21:46:42 +0600

    java practice

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,8 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email email = new Email("Max","Freeman");
+        System.out.printf(email.showInfo());
+    }
+}
